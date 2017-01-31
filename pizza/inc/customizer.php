997353691a9405a69497591c042e3df956e287c2
Natasha tenggoro <natashatenggoro@gmail.com>
2017-01-31 17:36:49 -0500

    First Commit

---
+++ pizza/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Pizza Theme Customizer
+ *
+ * @package Pizza
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function pizza_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'pizza_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function pizza_customize_preview_js() {
+	wp_enqueue_script( 'pizza_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'pizza_customize_preview_js' );
